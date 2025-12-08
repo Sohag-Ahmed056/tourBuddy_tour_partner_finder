28b323cc2f3f762d9bff86c80c25151019a90334
Sohag Ahmed <sohagahmed056@gmail.com>
2025-12-08 14:05:32 +0600

    change17

--- src/app/helper/fileUploader.ts
+++ src/app/helper/fileUploader.ts
@@ -7,7 +7,7 @@ import config from '../../config/index.js';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(process.cwd(), "/upload"))
+        cb(null, path.join(process.cwd(), "/uploads"))
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
