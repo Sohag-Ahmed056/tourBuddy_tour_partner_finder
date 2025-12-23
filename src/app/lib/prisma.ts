8f679304df7fd31244526d81ded863af27602d97
Sohag Ahmed <sohagahmed056@gmail.com>
2025-12-23 20:42:34 +0600

    last deploy stn4

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,6 +1,8 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import { PrismaClient } from "@prisma/client";
 
 
 const connectionString = `${process.env.DATABASE_URL}`
